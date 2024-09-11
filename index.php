1684adf628790dadc1365e8ab80dd93e91d5cb80
Babylene Rodriguez <142875371+BabyleneRodriguez@users.noreply.github.com>
2024-09-12 05:53:55 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,259 @@
+<!DOCTYPE html>
+<html>
+
+    <head>
+   
+		<title> BABYLENE'S PORTFOLIO </title>
+	
+		<link rel = "stylesheet" href = "css/bootstrap.min.css">
+		<link rel = "stylesheet" href = "css/customstyle.css">
+		<script src = "js/jquery.js"> </script>
+		<script src = "js/bootstrap.js"> </script>	
+			
+		</style>
+	
+    </head>
+   
+  
+
+    <body data-spy = "scroll" data-target = ".navbar">
+		
+		<nav class = "navbar navbar-inverse navbar-fixed-top">
+			<div class = "container-fluid">
+				<div class = "navbar-header">
+					<a class = "navbar-brand" href = "#"> Babylene G. Rodriguez </a>
+				</div>
+
+				<div class = "collapse navbar-collapse" id = "myNavbar">
+					<ul class = "nav navbar-nav navbar-right">
+						<li><a href = "#home"> Home </a></li>
+						<li><a href = "#about"> About </a></li>
+						<li><a href = "#skills"> Skills </a></li>
+						<li><a href = "#contact"> Contact </a></li>
+					</ul>
+				</div>
+			</div>
+		</nav>
+		
+
+
+		
+		<section id = "home" style = "background: url(image/homebackground.png); background-size: 100% 100%;" class = "homeHeader">
+			
+			<h1> MY PORTFOLIO </h1>
+			<p class = "lead"> Babylene G. Rodriguez </p>
+			
+		</section>
+		
+
+
+
+
+		<section id = "about" style = "background: url(image/sectionbg.png); background-size: 100% 100%;" class = "aboutSection">
+	
+			</br>
+			<div >
+				<img src = "image/BabyleneRodriguez.jpg" class = "img-circle center-block">	
+			</div>
+			</br>
+
+				<h1 class = "text-center"> ABOUT ME </h1>
+			</br>
+
+			<div>
+				<h4>
+				I am Babylene G. Rodriguez, 21 years old.
+				</br>
+				A Junior majoring in the Bachelor of Computer Science from the College of New Era University-Main Campus.
+				</br>
+				Photo editing, video editing, photography, web designing and programming are one of the things that fascinate me. 
+				</br>
+				Furthermore, when it comes to technical specialties I am skilled in Java, Javascript, PHP, HTML, CSS Bootstrap, and SQL.
+				</br> 
+				I am deeply resolved to refining my technical skills and constantly honing them by surfacing my fullest potential.
+				</br>
+				</br>
+				On the other hand, during my time in college, I accomplished my Oracle and Cognitive Class certifications. 
+				</h4>
+			</div> 
+			</br>
+		</section>
+		
+
+		
+	
+		<section id = "skills" style = "background: url(image/sectionbg.png); background-size: 100% 100%;" class = "skillsSection">
+			
+			<h1 class = "text-center"> MY SKILLS </h1>
+			</br>
+			</br>
+
+			<div class = "row">
+
+				<div class = "col-md-3">
+					<div class = "panel panel-primary">
+						<div class = "panel-heading">
+							<h4> Java Programmer </h4>
+						</div>
+						
+						<div class = "panel-body">
+							<img src = "image/java.png" class = "img-circle center-block">
+							<p class = "lead text-center"> I Do Java Programming </p>
+						</div>
+						
+						<div class = "panel-footer">
+						</div>
+					</div>
+				</div>
+
+
+				<div class = "col-md-3">
+					<div class = "panel panel-primary">
+						<div class = "panel-heading">
+							<h4> Javascript Programmer </h4>
+						</div>
+						
+						<div class = "panel-body">
+							<img src = "image/js.png" class = "img-circle center-block">
+							<p class = "lead text-center"> I Do Javascript Programming </p>
+						</div>
+						
+						<div class = "panel-footer">
+						</div>
+					</div>
+				</div>
+
+
+				<div class = "col-md-3">
+					<div class = "panel panel-primary">
+						<div class = "panel-heading">
+							<h4> PHP Programmer </h4>
+						</div>
+						
+						<div class = "panel-body">
+							<img src = "image/php.png" class = "img-circle center-block">
+							<p class = "lead text-center"> I Do PHP Programming </p>
+						</div>
+						
+						<div class = "panel-footer">
+						</div>
+					</div>
+				</div>
+
+
+				<div class = "col-md-3">
+					<div class = "panel panel-primary">
+						<div class = "panel-heading">
+							<h4> SQL Programmer</h4>
+						</div>
+						
+						<div class = "panel-body">
+							<img src = "image/sql.png" class = "img-circle center-block">
+							<p class = "lead text-center"> I Do SQL Programming </p>
+						</div>
+						
+						<div class = "panel-footer">
+						</div>
+					</div>
+				</div>
+
+
+				<div class = "col-md-3">
+					<div class = "panel panel-primary">
+						<div class = "panel-heading">
+							<h4> CSS Designer</h4>
+						</div>
+						
+						<div class = "panel-body">
+							<img src = "image/css.png" class = "img-circle center-block">
+							<p class = "lead text-center"> I Do CSS Designing </p>
+						</div>
+						
+						<div class = "panel-footer">
+						</div>
+					</div>
+				</div>
+
+				<div class = "col-md-3">
+					<div class = "panel panel-primary">
+						<div class = "panel-heading">
+							<h4> HTML Designer</h4>
+						</div>
+						
+						<div class = "panel-body">
+							<img src = "image/html.png" class = "img-circle center-block">
+							<p class = "lead text-center"> I Do HTML Designing </p>
+						</div>
+						
+						<div class = "panel-footer">
+						</div>
+					</div>
+				</div>
+
+
+				<div class = "col-md-3">
+					<div class = "panel panel-primary">
+						<div class = "panel-heading">
+							<h4> Photo Editor </h4>
+						</div>
+						
+						<div class = "panel-body">
+							<img src = "image/photo.png" class = "img-circle center-block">
+							<p class = "lead text-center"> I Do Photo Editing </p>
+						</div>
+						
+						<div class = "panel-footer">
+						</div>
+					</div>
+				</div>
+
+
+				<div class = "col-md-3">
+					<div class = "panel panel-primary">
+						<div class = "panel-heading">
+							<h4> Video Editor </h4>
+						</div>
+						
+						<div class = "panel-body">
+							<img src = "image/video.png" class = "img-circle center-block">
+							<p class = "lead text-center"> I Do Video Editing </p>
+						</div>
+						
+						<div class = "panel-footer">
+						</div>
+					</div>
+				</div>
+
+			</div>
+		
+		</section>
+		
+		
+		
+	
+		<section id = "contact" style = "background: url(image/sectionbg.png); background-size: 100% 100%;" class = "contactSection">
+		
+			<br/>
+			<br/>
+			<h1 class = "text-center"> CONTACT ME </h1>
+
+			
+			<h3> 
+				Gmail: <a href = "https://mail.google.com/"> babylenerdrgz@gmail.com </a>
+				<br/>
+				LinkedIn: <a href = "https://www.linkedin.com/"> Babylene </a>
+				<br/>
+				Contact Number: +0999999999999
+				<br/>
+				Facebook: Babylene
+				<br/>
+				Instagram: Babylene
+			</h3>	
+
+		</section>
+      
+	  
+    </body>  
+	
+</html>
+
